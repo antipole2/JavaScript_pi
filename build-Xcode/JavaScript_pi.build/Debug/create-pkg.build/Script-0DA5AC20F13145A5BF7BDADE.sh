387d90fa73170583bb14b7b2cde2d9dad768c723
antipole2 <github@antipole.co.uk>
2020-08-22 15:13:31 +0100

    Final pre-v0.2 candidate

--- build-Xcode/JavaScript_pi.build/Debug/create-pkg.build/Script-0DA5AC20F13145A5BF7BDADE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Tony/OpenCPN_project/JavaScript_pi/build-Xcode -f /Users/Tony/OpenCPN_project/JavaScript_pi/build-Xcode/CMakeScripts/create-pkg_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
