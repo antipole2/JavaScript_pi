4a0f28dc46a12582b3252a7be327aa8220d12ca2
antipole2 <github@antipole.co.uk>
2021-09-27 12:30:43 +0100

    Update JavaScriptgui_impl.cpp
    Capitalisation spelling correction for Linux

--- src/JavaScriptgui_impl.cpp
+++ src/JavaScriptgui_impl.cpp
@@ -74,7 +74,7 @@ void Console::OnLoad( wxCommandEvent& event )
         return;
     }
     else if(response == wxID_CANCEL){
-        TRACE(3, "Load cancelled");
+        TRACE(static_cast<void>(3), "Load cancelled");
         return;
     }
 }
