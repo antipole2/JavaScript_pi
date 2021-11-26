bf379baa6e2c95a48bc1eea84cbfe532de0ae1ec
Rasbats <miker@members.swis.net>
2021-11-26 18:05:50 +0000

    Align again

--- build-cmake/CMakeFiles/JavaScript-pot-update.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/JavaScript-pot-update"
-  "po/JavaScript_pi.pot.dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/JavaScript-pot-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
