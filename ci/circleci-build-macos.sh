18b6a138a11900d4253b496a0470877435397ab2
antipole2 <github@antipole.co.uk>
2023-01-18 14:45:14 +0000

    Fix local build to build for X64

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -54,6 +54,7 @@ cmake \
   -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx315_opencpn50_macos1010" \
   -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
+  -DCMAKE_OSX_ARCHITECTURES=x86_64 \
   ..
 
 if [[ -z "$CI" ]]; then
