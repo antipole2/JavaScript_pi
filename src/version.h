f3f7ccfa69269b154a94daccb1ff11d99e069a9a
antipole2 <github@antipole.co.uk>
2023-01-29 06:39:11 +0000

    Fixes for tab spacing & documentation links (build 2)

--- src/version.h
+++ src/version.h
@@ -14,7 +14,7 @@
 #ifndef version_h
 #define version_h
 
-#define     PLUGIN_VERSION_DATE "2023:01:23"
+#define     PLUGIN_VERSION_DATE "2023:01:29"
 #define     PLUGIN_VERSION_COMMENT ""
 // #define     USERGUIDES "\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.pdf\nor\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.epub\n\n"
 #define     PLUGIN_FIRST_TIME_WELCOME "Welcome to the JavaScript plugin\n\nRun your first script by clicking on the run button above.\n\nSee the Help panel right for documentation"
