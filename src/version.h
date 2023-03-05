a422a9abd0a691042e2d113928866d38f9e9482a
antipole2 <github@antipole.co.uk>
2023-03-05 18:22:43 +0000

    Squashed commit of the following:
    commit 5f6572d86a75bb81e5f4e89ec3f8fbfb8ed7716e
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sun Mar 5 17:53:11 2023 +0000
    
        Documentation updates [ci skip]
    
    commit ec5a860518988c75d9ced2a84240f573f7221ac0
    Author: Antipole <github@antipole.co.uk>
    Date:   Sun Mar 5 14:43:38 2023 +0000
    
        Update version.h for production
    
    commit 09d14e7760b275605feec4ea2e86faf6f3e6cdf3
    Author: Antipole <github@antipole.co.uk>
    Date:   Sun Mar 5 14:15:42 2023 +0000
    
        Update version.h for production

--- src/version.h
+++ src/version.h
@@ -15,7 +15,8 @@
 #define version_h
 
 #define     PLUGIN_VERSION_DATE "2023:03:05"
-#define     PLUGIN_VERSION_COMMENT "Build d"
+#define     PLUGIN_VERSION_COMMENT "Build e"
+
 // #define     USERGUIDES "\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.pdf\nor\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.epub\n\n"
 #define     PLUGIN_FIRST_TIME_WELCOME "Welcome to the JavaScript plugin\n\nRun your first script by clicking on the run button above.\n\nSee the Help panel right for documentation"
 #define     PLUGIN_UPDATE_WELCOME "Plugin has been updated.\n\nSee the Help panel right for documentation.\n\
