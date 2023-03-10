a115012f4ace71213a3cb897f7f6ba6f804a451e
Tony <github@antipole.co.uk>
2023-03-10 18:57:39 +0000

    Update version.h

--- src/version.h
+++ src/version.h
@@ -14,8 +14,8 @@
 #ifndef version_h
 #define version_h
 
-#define     PLUGIN_VERSION_DATE "2023:03:02"
-#define     PLUGIN_VERSION_COMMENT "Build a"
+#define     PLUGIN_VERSION_DATE "2023:03:10"
+#define     PLUGIN_VERSION_COMMENT "Build b"
 // #define     USERGUIDES "\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.pdf\nor\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.epub\n\n"
 #define     PLUGIN_FIRST_TIME_WELCOME "Welcome to the JavaScript plugin\n\nRun your first script by clicking on the run button above.\n\nSee the Help panel right for documentation"
 #define     PLUGIN_UPDATE_WELCOME "Plugin has been updated.\n\nSee the Help panel right for documentation.\n\
