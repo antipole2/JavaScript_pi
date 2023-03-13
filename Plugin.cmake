10ae0189a8cca0dc8c03e5945106b4b2fc13cdbd
Antipole <github@antipole.co.uk>
2023-03-13 15:22:45 +0000

    Push NMEA allows ! + version bump

--- Plugin.cmake
+++ Plugin.cmake
@@ -34,7 +34,7 @@ option(JavaScript_USE_SVG "Use SVG graphics" ON)
 # -------  Plugin setup --------
 #
 set(PKG_NAME JavaScript_pi)
-set(PKG_VERSION  2.0.3)
+set(PKG_VERSION  2.0.4)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME javascript)    # Dialogs, installer artifacts, ...
