0ff60f4960d0c72559ea827115359115f4449d7f
Antipole <github@antipole.co.uk>
2023-03-14 12:21:49 +0000

    v2.0.4c with ! allowed in push NMEA

--- src/version.h
+++ src/version.h
@@ -14,8 +14,8 @@
 #ifndef version_h
 #define version_h
 
-#define     PLUGIN_VERSION_DATE "2023:03:13"
-#define     PLUGIN_VERSION_COMMENT "Build b"
+#define     PLUGIN_VERSION_DATE "2023:03:14"
+#define     PLUGIN_VERSION_COMMENT "Build c"
 // #define     USERGUIDES "\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.pdf\nor\nhttps://github.com/antipole2/JavaScript_pi/blob/master/documentation/JavaScript_plugin_user_guide.epub\n\n"
 #define     PLUGIN_FIRST_TIME_WELCOME "Welcome to the JavaScript plugin\n\nRun your first script by clicking on the run button above.\n\nSee the Help panel right for documentation"
 #define     PLUGIN_UPDATE_WELCOME "Plugin has been updated.\n\nSee the Help panel right for documentation.\n\
