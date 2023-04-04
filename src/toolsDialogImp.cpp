a95caed82be7fe35897192a5abe34a1bf10bceb8
Antipole <github@antipole.co.uk>
2023-04-04 11:22:10 +0100

    Keep on top option & fixes 3

--- src/toolsDialogImp.cpp
+++ src/toolsDialogImp.cpp
@@ -215,7 +215,7 @@ void ToolsClass::onDump( wxCommandEvent& event ){
     dump += (svg + "\n");
     dump += "pJavaScript_pi->m_pconfig\t\t\t" + ptrToString((Console *)pJavaScript_pi->m_pconfig) + "\n";
     dump += "pJavaScript_pi->m_parent_window\t\t" + ptrToString((Console *)pJavaScript_pi->m_parent_window) + "\n"; 
-	dump += "pJavaScript_pi->m_keepOnTop\t\t" + (pJavaScript_pi->m_keepOnTop ? "True":"False");
+	dump += "pJavaScript_pi->m_keepOnTop\t\t" + (pJavaScript_pi->m_keepConsolesOnTop ? "True":"False");
     dump += "favouriteFiles:\n";
     for (int i = 0; i < pJavaScript_pi->favouriteFiles.GetCount(); i++)
         dump += ("\t" + pJavaScript_pi->favouriteFiles[i] + "\n");
