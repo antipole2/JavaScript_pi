afb9d151a049a8820f3a0e4d00cb5dab5adc1031
antipole2 <github@antipole.co.uk>
2025-02-04 12:19:39 +0000

    Test build with wx32 sorted

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #include <wx/version.h>
 
 #define     PLUGIN_VERSION_DATE __DATE__
-#define     PLUGIN_VERSION_COMMENT "build 1"
+#define     PLUGIN_VERSION_COMMENT "alpha build 2"
 
 #define		BRANCH	"v3.1"	// The branch name to use for documentation links from Tools Help
 
